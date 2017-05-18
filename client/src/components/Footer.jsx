681979850e320fc664aadbf2708eadfe701a0816
bhattprateek <prateekbhatt94@gmail.com>
2017-05-18 12:33:01 -0700

    (fix) Put author descriptions in center as well as surround author seciotn in card.

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
   <footer>
       <div className="container-fluid navbar-inverse">
-        <p className="text-muted">Made by Farrah Bousetta, Arseniy Kotov, and Helen Tang</p>
+        <h7 className="text-muted" id="footer-text">Made by Alex Seiden, Eric Pan, and Prateek Bhatt</h7>
       </div>
     </footer>
   );
