941e8f78d1a860d6805d90e21090e3456c339120
bhattprateek <prateekbhatt94@gmail.com>
2017-05-18 14:06:36 -0700

    (refactor)Remove card div.

--- client/src/components/Authors.jsx
+++ client/src/components/Authors.jsx
@@ -1,24 +1,24 @@
 const Authors = () => {
   return (
-    <div className="card">
+    <div>
       <div className="row">
           <h6 className="col-12"><b><u>ABOUT THE CREATORS</u></b></h6>
       </div>
       <div>
         <div className="col-lg-4">
-          <img src="images/farrah_bousetta.png" className="img-fluid bioImages" style={{ height: 200, width: 200}} alt="FARRAH PHOTO HERE"/>
+          <img src="images/farrah_bousetta.png" className="img-fluid img-responsive bioImages" style={{ height: 200, width: 200}} alt="FARRAH PHOTO HERE"/>
           <h6 id="author-des">Farrah Bousetta is an upcoming professional software engineer with previous experience at Facebook, Google and other prestegious tech companies. She gets stuff done. Her nickname is Feisty Farrah.</h6>
         </div>
       </div>
       <div>
         <div className="col-lg-4">
-          <img src="images/arseniy_kotov.png" className="img-fluid bioImages" style={{ height: 200, width: 200}} alt="ARSENIY PHOTO HERE"/>
+          <img src="images/arseniy_kotov.png" className="img-fluid img-responsive bioImages" style={{ height: 200, width: 200}} alt="ARSENIY PHOTO HERE"/>
           <h6 id="author-des">Arseniy Kotov is an all star programmer specializing in full stack developement.</h6>
         </div>
       </div>
       <div>
         <div className="col-lg-4">
-          <img src="images/helen_tang.png" className="img-fluid bioImages" style={{ height: 200, width: 200}} alt="HELEN PHOTO HERE"/>
+          <img src="images/helen_tang.png" className="img-fluid img-responsive bioImages" style={{ height: 200, width: 200}} alt="HELEN PHOTO HERE"/>
           <h6 id="author-des">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h6>
         </div>
       </div>
