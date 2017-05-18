bf11fe1344b73142e2167e4f2bdb9e00f77d7b0e
bhattprateek <prateekbhatt94@gmail.com>
2017-05-18 11:49:47 -0700

    (fix) Make website title and footer more responsive.

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,10 @@
 const Footer = () => {
   return (
-    <div className = "container-fluid-fullwidth">
-      <div className="navbar-default navbar-fixed-bottom">Made by Farrah Bousetta, Arseniy Kotov, and Helen Tang</div>
-    </div>
+  <footer>
+      <div className="container-fluid navbar-inverse">
+        <p className="text-muted">Made by Farrah Bousetta, Arseniy Kotov, and Helen Tang</p>
+      </div>
+    </footer>
   );
 };
 
