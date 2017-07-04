b2ebcc05eada947c030415638817cf01ecf7a042
Alex Seiden <alex.seiden@gmail.com>
2017-07-04 12:02:41 -0700

    (feat) change to trigger git update

--- client/src/components/CommentsList.jsx
+++ client/src/components/CommentsList.jsx
@@ -4,6 +4,7 @@ import CommentsEntry from './CommentsEntry.jsx';
 const CommentsList = ({ comments }) => {
   return (
     <div className="comments-list">
+
       {
         comments.slice(0).reverse().map(comment => {
           return <CommentsEntry key={comment.id} comment={comment} />;
